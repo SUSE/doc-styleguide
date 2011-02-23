f6ae900405eff67cb2ee91807f5497a424a27e59
jnweiger <jw@suse.de>
2011-02-23 11:02:46 +0000

    a scanner for typical marketing jargon. Omitting these words is often better
    git-svn-id: https://svn.opensuse.org/svn/opensuse-doc/trunk/documents/styleguide/en@2590 8ed6235d-527f-0410-a63b-cf03ab4a5eb2

---
+++ jsmeix/just-simple-easy.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+# Date: Wed, 23 Feb 2011 10:27:29 +0100 (CET)
+# From: Johannes Meixner <jsmeix@suse.de>
+# To: Juergen Weigert <jw@suse.de>
+# Subject: just-simple-easy - what else?
+# 
+# anbei just-simple-easy.lines was ich wie folgt erzeugt habe
+# (mit einem ad-hoc Versuch via sed einige Falsch-Positive zu vermeiden)
+# wobei ich 638 Zeilen fand, die 'just|simple|simply|easy|easily' enthalten,
+# die vermutlich immer noch einige Falsch-Positive beinhalten
+# (etwa "A simple test script" oder "If you prefer a simple way"):
+# ---------------------------------------------------------------------------
+# $ svn checkout \
+# svn://svn.berlios.de/opensuse-doc/trunk/documents/distribution/en/xml \
+# opensuse-doc.trunk.documents.distribution.en.xml
+# 
+# $ cd opensuse-doc.trunk.documents.distribution.en.xml
+
+for f in *.xml ; \
+do echo $f ; \
+   cat $f \
+   | sed -e '/<!--.*-->/d' \
+   | sed -e '/<!--/,/-->/d' \
+   | sed -e '/<remark>.*<\/remark>/d' \
+   | sed -e '/<remark>/,/<\/remark>/d' \
+   | egrep -n -T -i 'just|simple|simply|easy|easily' \
+   | egrep -i -v 'simplelist>|SIMPLEREQ>|SIMPLERSP>|adjust' ; \
+   echo =============================================================== ; \
+done >just-simple-easy.lines
+
+# $ egrep 'just|simple|simply|easy|easily' just-simple-easy.lines | wc -l
+# 638
